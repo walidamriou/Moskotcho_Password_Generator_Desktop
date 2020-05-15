da4303cbf041f5b24d55a4aa54c11d2a22e9a3c4
Walid Amriou <walidamriou@gmail.com>
2020-05-15 08:46:19 +0200

    remove menu

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const path = require('path')
 
 function createWindow () {
   // Create the browser window.
+
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -15,6 +16,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+  mainWindow.setMenuBarVisibility(false)
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
