9eef9d1e2203698261a0ebf91a79d87b265f4f8a
Walid Amriou <walidamriou@gmail.com>
2020-06-11 20:12:36 +0200

    Update

--- version.js
+++ version.js
@@ -10,10 +10,3 @@ function readJsonFile(file, callback) {
     rawFile.send(null);
 }
 
-readJsonFile("https://github.com/walidamriou/Moskotcho_Password_Generator_Desktop/blob/master/current_version.json", function(text){
-    var data = JSON.parse(text);
-    console.log(data);
-    alert(data[0]);
-    //var elem = document.getElementById("datadisplay");
-    //elem.innerHTML = data.data['id']; //we want to read: "id": "123664" 
-});
